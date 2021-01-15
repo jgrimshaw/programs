ea6163edcacc23b7652adeb3cda9100e19be92aa
jgrimshaw <muchstudio@hotmail.com>
2021-01-15 11:24:25 -0700

    initial setup

---
+++ src/components/Programs.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+
+export const Programs = () => {
+    // const [program, setProgram] = useState([])
+
+    useEffect(() => {
+        fetch('/api').then(response => {
+            if(response.ok) {
+                return response.json()
+            }
+        }).then(data => console.log(data))
+    }, [])
+
+    return <div>programs</div>
+}
